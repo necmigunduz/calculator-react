3757e835bfde4b4663e22138551d3e8eb916cbcc
necmigunduz <necmigunduz@gmail.com>
2021-06-17 14:07:39 +0300

    Fix buttons

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,36 +1,37 @@
 import React from 'react';
+import Button from './Button';
 
 export default function ButtonPannel() {
   return (
     <div className="button-panel">
       <div className="buttons">
-        <button className="AC" aria-label="AC" type="button" />
-        <button className="+/-" aria-label="+/-" type="button" />
-        <button className="%" aria-label="%" type="button" />
-        <button className="/" aria-label="/" type="button" />
+        <Button className="AC" />
+        <Button className="+/-" />
+        <Button className="%" />
+        <Button className="/" />
+      </div>
+      <div className="Buttons">
+        <Button className="7" />
+        <Button className="8" />
+        <Button className="9" />
+        <Button className="x" />
       </div>
       <div className="buttons">
-        <button className="7" aria-label="7" type="button" />
-        <button className="8" aria-label="8" type="button" />
-        <button className="9" aria-label="9" type="button" />
-        <button className="x" aria-label="x" type="button" />
-      </div>
-      <div className="buttons">
-        <button className="4" aria-label="4" type="button" />
-        <button className="5" aria-label="5" type="button" />
-        <button className="6" aria-label="6" type="button" />
-        <button className="-" aria-label="-" type="button" />
+        <Button className="4" />
+        <Button className="5" />
+        <Button className="6" />
+        <Button className="-" />
       </div>
       <div className="butons">
-        <button className="1" aria-label="1" type="button" />
-        <button className="2" aria-label="2" type="button" />
-        <button className="3" aria-label="3" type="button" />
-        <button className="+" aria-label="+" type="button" />
+        <Button className="1" />
+        <Button className="2" />
+        <Button className="3" />
+        <Button className="+" />
       </div>
       <div className="buttons">
-        <button className="0" aria-label="0" type="button" />
-        <button className="." aria-label="0" type="button" />
-        <button className="=" aria-label="0" type="button" />
+        <Button className="0" />
+        <Button className="." />
+        <Button className="=" />
       </div>
     </div>
   );
